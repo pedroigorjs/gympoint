17e858a14093fe4ce41d6b8bb39b5a821f014c9e
Pedro Sampaio <pedroigorjs@gmail.com>
2020-03-12 10:13:47 -0300

    Setup cors with express server

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import './database';
 
@@ -14,6 +15,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(cors());
   }
 
   routes() {
