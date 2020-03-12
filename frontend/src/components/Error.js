83cb479f234eff059117e9069d78a91c3d8445bc
Pedro Sampaio <pedroigorjs@gmail.com>
2020-03-12 10:16:37 -0300

    Create Error component

---
+++ frontend/src/components/Error.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Code = styled.h1`
+  font-size: 6rem;
+  color: #ee4d64;
+`;
+
+export const Message = styled.p`
+  font-size: 3rem;
+  color: #444;
+`;
